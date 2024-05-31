20557bd13e88b9fdd3177033d0bba9e84cb28f9c
Munkhjin11x <147012832+Munkhjin11x@users.noreply.github.com>
2024-05-31 17:54:49 +0800

    s

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import { connectDataBase } from "./utils/data";
+import dotenv from "dotenv";
+import cors from "cors";
 connectDataBase();
+dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(cors());
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
--- backend/src/utils/data.ts
+++ backend/src/utils/data.ts
@@ -1,13 +1,16 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 const connectDataBase = async () => {
-    try {
-        const MONGODB_URI: string = process.env.MONGODB_URI || ""
-        await mongoose.connect(MONGODB_URI);
-        console.log('success')
-    } catch (error: unknown) {
-        console.log(error)
-        throw new Error('unsuccess')
-    }
+  try {
+    const MONGODB_URI: string =
+      process.env.MONGODB_URI ||
+      "mongodb+srv://Munkhjin:99279353Aa!@cluster0.dhvxd3q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  console.log(MONGODB_URI)
 
-}
-export { connectDataBase }
+    await mongoose.connect(MONGODB_URI);
+    console.log("success");
+  } catch (error: unknown) {
+    console.log(error);
+    throw new Error("unsuccess");
+  }
+};
+export { connectDataBase };
